5100397fb55c32fa12475dd329f3d426357fa8f0
Kafil Nasdami <kafil.nasdami@gmail..com>
2024-02-21 17:29:18 +0100

    add a qr code and copy to clipboard

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { PasswordGenerator } from "@/components/component/password-generator";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <PasswordGenerator />;
+      <PasswordGenerator />
     </div>
   );
 }
--- components/component/password-generator.tsx
+++ components/component/password-generator.tsx
@@ -3,8 +3,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { Button } from "@/components/ui/button"; // Assuming toast and Button are correctly imported
-
 import {
   Card,
   CardContent,
@@ -15,12 +13,10 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-import QRCode from 'qrcode.react';
+import QRCode from "qrcode.react";
 import { useState } from "react";
 import { Slider } from "../ui/slider";
 import { toast } from "../ui/use-toast";
-
-
 
 const defaultValues = {
   length: 12,
@@ -54,135 +50,150 @@ const generatePassword = (data: z.infer<typeof PasswordSchema>) => {
   if (data.numbers) characterSet += numberChars;
   if (data.special) characterSet += specialChars;
 
-  if (!data.uppercase && !data.lowercase && !data.numbers && !data.special) {
-    toast({
-      title: "Invalid Password Criteria",
-      description: "Please select at least one character set to use",
-    });
-    return;
-  }
-
   let password = "";
   for (let i = 0; i < data.length; i++) {
     const randomIndex = Math.floor(Math.random() * characterSet.length);
     password += characterSet[randomIndex];
   }
 
+  return password;
+};
+const generateSuccessToast = (password: string) =>
   toast({
     title: "Password generated",
     description:
-      "Your password has been generated and is ready to use: " + password,
+      "Your password has been generated and is copied to the clipboard: " +
+      password,
   });
-  return password;
+
+const copyToClipboard = (password: string) => {
+  navigator.clipboard
+    .writeText(password)
+    .then(() => {
+      // console.log('Password copied to clipboard!'+password)
+    })
+    .catch((err) => {
+      console.error("Failed to copy:", err);
+    });
 };
 
 export function PasswordGenerator() {
-  const { control, handleSubmit, watch, setValue } = useForm<
-    z.infer<typeof PasswordSchema>
-  >({
+  const { control, watch, setValue } = useForm<z.infer<typeof PasswordSchema>>({
     resolver: zodResolver(PasswordSchema),
-    defaultValues
+    defaultValues,
   });
-  
-  const [generatedPassword, setGeneratedPassword] = useState(generatePassword(defaultValues));
+
+  const [generatedPassword, setGeneratedPassword] = useState<string>(
+    generatePassword(defaultValues)
+  );
 
   return (
-
-    <div className="w-full h-screen flex gap-1 items-center justify-center">
-    <Card className="w-1/2 max-w-3xl mx-auto">
-      <CardHeader className="space-y-1">
-        <CardTitle>Generate Password</CardTitle>
-        <CardDescription>
-          Customize your password criteria and length
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={handleSubmit((e)=>setGeneratedPassword(generatePassword(e)))} className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="password">Your Password</Label>
-            <Input
-              id="password"
-              readOnly
-              type="text"
-              value={generatedPassword}
-            />
-          </div>
-          <div className="grid grid-cols-2 items-center gap-4">
-            <div className="col-span-2 space-y-2">
-              <Label htmlFor="length">Password Length</Label>
+    <div className="flex justify-center items-center gap-4 w-full px-10">
+      <Card className="w-3/4">
+        <CardHeader className="space-y-1">
+          <CardTitle>Generate Password</CardTitle>
+          <CardDescription>
+            Customize your password criteria and length
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            onChange={() => {
+              setGeneratedPassword(generatePassword(watch()));
+              copyToClipboard(generatedPassword);
+              generateSuccessToast(generatedPassword);
+            }}
+            className="space-y-4"
+          >
+            <div className="space-y-2">
+              <Label htmlFor="password">Your Password</Label>
+              <Input
+                id="password"
+                readOnly
+                type="text"
+                value={generatedPassword}
+              />
+            </div>
+            <div className="grid grid-cols-2 items-center gap-4">
+              <div className="col-span-2 space-y-2">
+                <Label htmlFor="length">Password Length</Label>
+                <Controller
+                  name="length"
+                  control={control}
+                  render={({ field }) => (
+                    <Slider
+                      defaultValue={[field.value]}
+                      min={1}
+                      max={128}
+                      step={1}
+                      onValueChange={(values) => setValue("length", values[0])} // Assuming Slider calls onValueChange with the new value
+                      className="w-full" // Adjust your slider's width as needed
+                    />
+                  )}
+                />
+              </div>
+              <Label htmlFor="uppercase">Uppercase</Label>
               <Controller
-                name="length"
+                name="uppercase"
                 control={control}
                 render={({ field }) => (
-                  <Slider
-                    defaultValue={[field.value]}
-                    min={1}
-                    max={128}
-                    step={1}
-                    onValueChange={(values) => setValue("length", values[0])} // Assuming Slider calls onValueChange with the new value
-                    className="w-full" // Adjust your slider's width as needed
+                  <Switch
+                    id="uppercase"
+                    checked={field.value}
+                    onCheckedChange={(checked) =>
+                      setValue("uppercase", checked)
+                    }
+                  />
+                )}
+              />
+
+              <Label htmlFor="lowercase">Lowercase</Label>
+              <Controller
+                name="lowercase"
+                control={control}
+                render={({ field }) => (
+                  <Switch
+                    id="lowercase"
+                    checked={field.value}
+                    onCheckedChange={(checked) =>
+                      setValue("lowercase", checked)
+                    }
+                  />
+                )}
+              />
+
+              <Label htmlFor="numbers">Numbers</Label>
+              <Controller
+                name="numbers"
+                control={control}
+                render={({ field }) => (
+                  <Switch
+                    id="numbers"
+                    checked={field.value}
+                    onCheckedChange={(checked) => setValue("numbers", checked)}
+                  />
+                )}
+              />
+
+              <Label htmlFor="special">Special Characters</Label>
+              <Controller
+                name="special"
+                control={control}
+                render={({ field }) => (
+                  <Switch
+                    id="special"
+                    checked={field.value}
+                    onCheckedChange={(checked) => setValue("special", checked)}
                   />
                 )}
               />
             </div>
-            <Label htmlFor="uppercase">Uppercase</Label>
-            <Controller
-              name="uppercase"
-              control={control}
-              render={({ field }) => (
-                <Switch
-                  id="uppercase"
-                  checked={field.value}
-                  onCheckedChange={(checked) => setValue("uppercase", checked)}
-                />
-              )}
-            />
-
-            <Label htmlFor="lowercase">Lowercase</Label>
-            <Controller
-              name="lowercase"
-              control={control}
-              render={({ field }) => (
-                <Switch
-                  id="lowercase"
-                  checked={field.value}
-                  onCheckedChange={(checked) => setValue("lowercase", checked)}
-                />
-              )}
-            />
-
-            <Label htmlFor="numbers">Numbers</Label>
-            <Controller
-              name="numbers"
-              control={control}
-              render={({ field }) => (
-                <Switch
-                  id="numbers"
-                  checked={field.value}
-                  onCheckedChange={(checked) => setValue("numbers", checked)}
-                />
-              )}
-            />
-
-            <Label htmlFor="special">Special Characters</Label>
-            <Controller
-              name="special"
-              control={control}
-              render={({ field }) => (
-                <Switch
-                  id="special"
-                  checked={field.value}
-                  onCheckedChange={(checked) => setValue("special", checked)}
-                />
-              )}
-            />
-          </div>
-          <Button type="submit">Generate Password</Button>
-        </form>
-      </CardContent>
-    </Card>
-    <div id='#qr' className="1/2 border border-e-red-200">
-    <QRCode value={generatedPassword} />
+            {/* <Button type="submit">Generate Password</Button> */}
+          </form>
+        </CardContent>
+      </Card>
+      <div className="w-1/4 flex items-center justify-center">
+        <QRCode value={generatedPassword} />
       </div>
     </div>
   );
